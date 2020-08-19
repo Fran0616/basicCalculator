612e2f590536801de270e7b66398d4feda8ab60e
Fran0616 <59398607+Fran0616@users.noreply.github.com>
2020-08-18 21:09:41 -0400

    Add files via upload

---
+++ basicCalculator.py
@@ -0,0 +1,17 @@
+#the objective of this project is to evaluate a simple algebraic expression of two value. 
+#float value to calculate
+a = float(input("Enter a  value:"))
+
+#float value to calculate 
+b = float(input("Enter a  value:"))
+
+# output the result of addition here
+print("The sum is " + str(a + b))
+# output the result of subtraction here
+print("The difference is " + str(a-b))
+# output the result of multiplication here
+print("The product is " + str(a*b))
+# output the result of division here
+print("The quotient is " + str(a/b))
+
+print("\nThat's all!")
